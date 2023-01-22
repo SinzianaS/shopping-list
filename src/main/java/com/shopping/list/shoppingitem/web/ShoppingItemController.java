5e303fa992239ce816208df8459fb8daf2635e16
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-01-22 22:04:38 +0200

    implemented show list of items

--- src/main/java/com/shopping/list/shoppingitem/web/ShoppingItemController.java
+++ src/main/java/com/shopping/list/shoppingitem/web/ShoppingItemController.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/")
 public class ShoppingItemController {
     private final ShoppingItemRepository repository;
+
     public ShoppingItemController(ShoppingItemRepository repository) {
         this.repository = repository;
     }
@@ -21,13 +25,27 @@ public class ShoppingItemController {
     @GetMapping
     public String index(Model model) {
         model.addAttribute("item", new ShoppingItemFormData());
+        model.addAttribute("shoppingItems", getShoppingItems());
         model.addAttribute("totalNumberOfItems", repository.count());
         return "index";
     }
 
     @PostMapping
-       public String addNewShoppingItem(@Valid @ModelAttribute("item") ShoppingItemFormData formData) {
-       repository.save(new ShoppingItem(formData.getTitle(), false));
-       return "redirect:/";
+    public String addNewShoppingItem(@Valid @ModelAttribute("item") ShoppingItemFormData formData) {
+        repository.save(new ShoppingItem(formData.getTitle(), false));
+        return "redirect:/";
     }
+
+    private List<ShoppingItemDto> getShoppingItems() {
+        return repository.findAll()
+                .stream()
+                .map(shoppingItem -> new ShoppingItemDto(shoppingItem.getId(),
+                                                         shoppingItem.getTitle(),
+                                                         shoppingItem.isCompleted()))
+                .collect(Collectors.toList());
+    }
+    public static record ShoppingItemDto(long id, String title, boolean completed) {
+    }
+
+
 }
