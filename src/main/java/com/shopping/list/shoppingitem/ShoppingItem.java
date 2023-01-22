dfbea4a37294eea6407b45c67edfb4f48ee853f6
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-01-22 20:14:40 +0200

    added model, repository and controller

---
+++ src/main/java/com/shopping/list/shoppingitem/ShoppingItem.java
@@ -0,0 +1,50 @@
+package com.shopping.list.shoppingitem;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class ShoppingItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private boolean completed;
+
+    protected ShoppingItem() {
+
+    }
+
+    public ShoppingItem(String title,
+                    boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
+
